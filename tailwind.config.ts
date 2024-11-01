b0606ead7dea7c3ba58b071fcec55727ad3631bd
Aysha Uddin <aysha.uddin29@gmail.com>
2024-11-01 00:51:32 +0000

    Configure eslint + tailwind colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,37 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        blue: {
+          custom1: '#00338D',
+          custom2: '#005EB8',
+          custom3: '#0071b6',
+        },
+        purple: {
+          custom1: '#483698',
+          custom2: '#470A68',
+          custom3: '#6D2077',
+        },
+        green: {
+          custom1: '#007E7D',
+          custom2: '#009A44',
+          custom3: '#43B02A',
+        },
+        pink: { custom1: '#C6007E' },
+        black: { custom1: '#333D49' },
+        gray: { custom1: '#F8F9FD' },
+        orange: { custom1: '#F68D2E' },
+        white: { custom1: '#F7F7F8' },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config

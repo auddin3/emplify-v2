779c7f55e5f3158ea4e6331d39aa908bde9271d0
Aysha Uddin <aysha.uddin29@gmail.com>
2024-11-27 01:38:40 +0000

    RM empty classname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const Home = () => {
   const clipboard = useClipboard({ value })
 
   return (
-    <div className="">
+    <div>
       <Navbar />
       <div className='bg-gradient-to-r from-blue-custom2 to-purple-custom4 space-y-20 px-28 py-24 text-center'>
         <h1 className='text-6xl text-white-custom1 font-bold'>
